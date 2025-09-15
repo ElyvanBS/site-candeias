220d376b6bc34feea0a31c4de05d766f32a75dc7
elyvanbs <elyvanbs@gmail.com>
2025-09-15 17:58:34 -0500

    feat: insere header no app.html de index

---
+++ candeias-2.0/src/app/components/nucleos/nucleos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nucleos',
+  imports: [],
+  templateUrl: './nucleos.html',
+  styleUrl: './nucleos.css'
+})
+export class Nucleos {
+
+}
